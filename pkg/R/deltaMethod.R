be733f43c77d5c38f96a53e738eb41bac2a1e5a6
sanfordweisberg (Dr ) <sandy@umn.edu>
2009-10-27 16:04:10 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/art@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/deltaMethod.R
+++ pkg/R/deltaMethod.R
@@ -1,92 +1,80 @@
-deltaMethod <- function(object,...)
-  {UseMethod("deltaMethod")}
-  
-deltaMethod.default <- function(object,g,var,...){
-   if(!is.character(g)) stop("The argument 'g' must be a character string")
-   coefest <- object
-   charSubst <- function(x,oldchar) { 
-      sapply(x,function(x) paste(unlist(strsplit(x,oldchar)),collapse="_"))}
-   func <- g  
-# remove characters from g and para names that would cause D to fail 
-   g <- charSubst(g,oldchar=":")
-   for (c in c(":","\\^"," ")){
-          names(coefest) <- charSubst(names(coefest),oldchar=c)}
-   g <- parse(text=g)
-   q <- length(coefest)
-   for(i in 1:q) {assign(names(coefest)[i], coefest[i])}
-   est<-eval(g) 
-   names(est)<-NULL
-# derivative of function g of parameters
-   gd<-NULL
-   for(i in 1:q) {gd<-c(gd, eval(D(g, names(coefest)[i])))}
-# compute se          
-   se.est<-as.vector(sqrt(t(gd) %*% var %*% gd))
-# output
-   data.frame(Estimate=est, SE=se.est, row.names=c(func))
+deltaMethod <-
+function (object, ...) 
+{
+    UseMethod("deltaMethod")
 }
 
-### lm, glm, and others with unnamed parameters:
-deltaMethod.lm<-function(object,g,var=vcov,parameterPrefix="b",...)
+deltaMethod.default <-
+function (object, g, var, func=g, ...) 
 {
-   para <- coef(object)
-   var <- if(is.function(var)) var(object) else var
-   names(para) <- if ("(Intercept)" %in% names(para))
-    paste(parameterPrefix,0:(length(para)-1),sep="") else
-    paste(parameterPrefix,1:length(para),sep="")
-   deltaMethod.default(para,g,var)}
-   
+    if (!is.character(g)) 
+        stop("The argument 'g' must be a character string")
+    coefest <- object
+    charSubst <- function(x, oldchar) {
+        sapply(x, function(x) paste(unlist(strsplit(x, oldchar)), 
+            collapse = "_"))
+    }
+    g <- charSubst(g, oldchar = ":")
+    for (c in c(":", "\\^", " ")) {
+        names(coefest) <- charSubst(names(coefest), oldchar = c)
+    }
+    g <- parse(text = g)
+    q <- length(coefest)
+    for (i in 1:q) {
+        assign(names(coefest)[i], coefest[i])
+    }
+    est <- eval(g)
+    names(est) <- NULL
+    gd <- NULL
+    for (i in 1:q) {
+        gd <- c(gd, eval(D(g, names(coefest)[i])))
+    }
+    se.est <- as.vector(sqrt(t(gd) %*% var %*% gd))
+    data.frame(Estimate = est, SE = se.est, row.names = c(func))
+}
+
+deltaMethod.lm <-
+function (object, g, var = vcov, parameterPrefix = "b", ...) 
+{
+    metas <- c("(", ")", "[", "]", "{", "}", ".", "*", "+", "^", "$", ":", "|")
+    metas2 <- paste("\\", metas, sep="")
+    metas3 <- paste("\\\\", metas, sep="")
+    para <- coef(object)
+    para.names <- names(para)
+    for (i in seq(along=metas))
+        para.names <- gsub(metas2[i], metas3[i], para.names) # fix up metacharacters
+    para.order <- order(nchar(para.names), decreasing=TRUE) 
+    para.names <- para.names[para.order] # avoid partial-name substitution
+    std.names <- if ("(Intercept)" %in% names(para)) 
+            paste(parameterPrefix, 0:(length(para) - 1), sep = "")
+        else paste(parameterPrefix, 1:length(para), sep = "")
+    std.names.ordered <- std.names[para.order]
+    func <- g
+    for (i in seq(along=para.names)){
+        g <- gsub(para.names[i], std.names.ordered[i], g) 
+        }
+    var <- if (is.function(var)) 
+        var(object)
+    else var
+    names(para) <- std.names
+    deltaMethod.default(para, g, var, func)
+}
+
 # nls has named parameters so parameterPrefix is ignored
 deltaMethod.nls<-function(object, g, var=vcov,...)
 {
    var <- if(is.function(var)) var(object)
    deltaMethod.default(coef(object),g,var)}
   
-   
-deltaMethod.lmList <- function(object,g,var=vcov,parameterPrefix="b",...){
-  out <- NULL
-  for (j in 1:length(object)){
-   ans <- deltaMethod(object[[j]],g,var=var,parameterPrefix="b")
-   rownames(ans)[1]<-paste(names(object)[j],rownames(ans)[1])
-   out <- rbind(out,ans)
-   }
-   out} 
-   
-deltaMethod.nlsList <- function(object,g,var=vcov,...){
-  out <- NULL
-  for (j in 1:length(object)){
-   ans <- deltaMethod(object[[j]],g,var=vcov)
-   rownames(ans)[1]<-paste(names(object)[j],rownames(ans)[1])
-   out <- rbind(out,ans)
-   }
-   out} 
-   
-deltaMethod.lme <-  function(object,g,var=vcov,parameterPrefix="b",...)
-{  para <- fixef(object)
-   var <- if(is.function(var)) var(object)
-   names(para) <- if ("(Intercept)" %in% names(para))
-    paste(parameterPrefix,0:(length(para)-1),sep="") else
-    paste(parameterPrefix,1:length(para),sep="")
-   deltaMethod.default(para,g,var)}
-   
-deltaMethod.nlme <-  
-function(object,g,var=vcov,...)
-{  para <- fixef(object)
-   var <- if(is.function(var)) var(object)
-   deltaMethod.default(para,g,var)}
-   
-deltaMethod.mer <-  
-function(object,g,var=vcov,parameterPrefix="b",...)
-{  para <- fixef(object)
-   var <- if(is.function(var)) var(object)
-   names(para) <- if ("(Intercept)" %in% names(para))
-    paste(parameterPrefix,0:(length(para)-1),sep="") else
-    paste(parameterPrefix,1:length(para),sep="")
-   deltaMethod.default(para,g,var)}
-   
 deltaMethod.multinom<-function(object,g,var=vcov,parameterPrefix="b",...)
 {
    out <- NULL
    coefs <- coef(object)
+   if (!is.matrix(coefs)) {
+     nn <- names(coefs)
+     coefs <- matrix(coefs,nrow=1)
+     colnames(coefs) <- nn
+     }
    nc <- dim(coefs)[2]
   for (i in 1:dim(coefs)[1]){
    para <- coefs[i,]
@@ -99,12 +87,13 @@ deltaMethod.multinom<-function(object,g,var=vcov,parameterPrefix="b",...)
    }
    out}
    
-deltaMethod.polr<-function(object,g,var=vcov,parameterPrefix="b",...)
+deltaMethod.polr<-function(object,g,var=vcov,...)
 {
-   para <- coef(object)
-   var <- if(is.function(var)) var(object)[1:length(para),1:length(para)]
-   names(para) <- if ("(Intercept)" %in% names(para))
-    paste(parameterPrefix,0:(length(para)-1),sep="") else
-    paste(parameterPrefix,1:length(para),sep="")
-   deltaMethod.default(para,g,var)}
+   sel <- 1:(length(coef(object)))
+   var <- if(is.function(var)) var(object)[sel,sel]
+   deltaMethod.lm(object,g,var,...)
+   }
+
+
    
+      
